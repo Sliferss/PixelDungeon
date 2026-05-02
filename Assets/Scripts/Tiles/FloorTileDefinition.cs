99db743051693cb18a9c00d1c5336d35b859080d
AhmadNaseryHolloway <94611606+AhmadNaseryHolloway@users.noreply.github.com>
2026-05-02 11:30:06 +0100

    commit

--- Assets/Scripts/Tiles/FloorTileDefinition.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "Tiles/Floor")]
-public class FloorTileDefinition : TileDefinition
-{
-    public AudioClip StepSound;
-}
--- Assets/Scripts/Tiles/TileDefinition.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public abstract class TileDefinition : ScriptableObject
-{
-    public string Id;
-    public Sprite Sprite;
-    public bool IsWalkable = true;
-    public int MovementCost = 1;
-}
